ab42e821da593b8a92e18894b7aeeaa91c19f6a8
Matt Seidholz <mseidholz@gmail.com>
2024-03-09 16:43:05 -0500

    Git add is working now too - though silently.

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite)
+vim.keymap.set("n", "<leader>ga", function()
+	vim.cmd("Git add .")
+end)
 vim.keymap.set("n", "<leader>gc", function()
 	vim.cmd("Git commit")
 end)
