2488aa3a22f56c14e14e1cda0ea63ec7198c3977
Matt Seidholz <mseidholz@gmail.com>
2024-03-16 10:23:59 -0400

    add lsp bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,13 +2,48 @@ local lsp_zero = require("lsp-zero")
 
 
 
+local cmp = require('cmp');
+local cmp_select = require('lsp-zero').cmp_select;
+
+cmp.setup({
+
+	mapping = cmp.mapping.preset.insert({
+
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+  });
+});
+
+
+
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+ vim.keymap.set("n",  "gD", function() vim.lsp.buf.declaration() end, opts)
+ vim.keymap.set("n",  "gt", function() vim.lsp.buf.type_definition() end, opts)
+ vim.keymap.set("n",  "gi", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+end)
+
+
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	-- Replace the language servers listed here 
-	-- with the ones you want to install
-	ensure_installed = {'tsserver', 'eslint', 'solargraph', 'lua_ls' },
+	ensure_installed = {'tsserver', 'eslint', 'solargraph', 'lua_ls', 'jsonls', 'marksman', 'yamlls', 'bashls', 'cssls' },
 	handlers = {
 		lsp_zero.default_setup,
 	},
+
+
 })
 
