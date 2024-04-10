c07bc4e1b5863b665704bc35a9a3970799c94710
Matt Seidholz <matthew.seidholz@energyhub.com>
2024-04-10 14:06:12 -0400

    add relative path copying

--- lua/matt/remap.lua
+++ lua/matt/remap.lua
@@ -16,3 +16,24 @@ vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+
+
+function copy_relative_path()
+  -- Find the root directory
+  local root_dir = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+  if vim.v.shell_error ~= 0 then
+    print("Not in a git repository")
+    return
+  end
+
+  -- Get the absolute path of the current buffer
+  local abs_path = vim.fn.expand('%:p')
+
+  -- Get the relative path
+  local rel_path = abs_path:sub(#root_dir + 2)
+
+  -- Copy the relative path to the clipboard
+  vim.fn.setreg('+', rel_path)
+end
+
+vim.api.nvim_set_keymap('n', '<leader>k', ':lua copy_relative_path()<CR>', {noremap = true, silent = true})
