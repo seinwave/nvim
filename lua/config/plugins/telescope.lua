ad9f283d1bb0bbdd55cfe6dcbded07543a509e27
Matt Seidholz <matthew.seidholz@energyhub.com>
2025-04-23 11:27:32 -0400

    add multigrep, add diffview

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require "config.telescope.multigrep".setup()
+    end
   },
 }
