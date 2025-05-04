b9e0738d8e0f63b3de471dd63975154341e5f5aa
Matt Seidholz <mseidholz@gmail.com>
2025-05-04 07:37:10 -0400

    fix clipboard, root

--- lua/config/keymaps/root.lua
+++ lua/config/keymaps/root.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil --float<CR>", { desc = "Open Oil (float) at CWD" })
+vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open Oil (float) at CWD" })
 
 -- lua execution
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
