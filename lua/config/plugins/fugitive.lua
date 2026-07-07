4d505c2510abcf5708fa37fdc2b0f7d9e475e712
Matt Seidholz <matthew.seidholz@energyhub.com>
2026-07-07 14:44:50 -0400

    revert

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -1,23 +1,3 @@
 return {
-  "tpope/vim-fugitive",
-  config = function()
-    -- Move status and commit windows to a vertical split on the right
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "fugitive", "gitcommit" },
-      callback = function()
-        vim.cmd("wincmd L")
-      end,
-    })
-
-    -- Make :Gdiff open a vertical diff split
-    vim.api.nvim_create_autocmd("BufReadPost", {
-      pattern = "fugitive://*",
-      callback = function()
-        vim.cmd("wincmd L")
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>gp", ":Git pull<CR>", { silent = true, desc = "Git pull" })
-    vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { silent = true, desc = "Git diff" })
-  end,
+  "tpope/vim-fugitive"
 }
